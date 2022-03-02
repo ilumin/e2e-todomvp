9084e7ac9757abb6e41a54db22b913602d844f3d
O Teerasak Vichadee <iolumin@gmail.com>
2022-03-02 16:40:40 +0700

    init playwright project

---
+++ playwright.config.ts
@@ -0,0 +1,29 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  testDir: "./specs",
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    actionTimeout: 0,
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+  ],
+};
+
+export default config;
